2ce8a6418328bf1a5b3625b58f545f43a6fd4857
SENSENGO\Shelly.Chen <Shelly.Chen@ehsn.com.tw>
2021-06-08 14:57:31 +0800

    project init

---
+++ src/container/ReservationBooking/ReservationBooking.js
@@ -0,0 +1,11 @@
+import "./ReservationBooking.scss";
+
+function ReservationBooking() {
+  return (
+    <div>
+      <div>ReservationBooking Page</div>
+    </div>
+  );
+}
+
+export default ReservationBooking;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+// import Home from "./Home";
+import reportWebVitals from "./reportWebVitals";
+import ReservationBooking from "./container/ReservationBooking/ReservationBooking";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReservationBooking />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
