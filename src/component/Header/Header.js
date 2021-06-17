f3225e8475d389acfe11e9a0a3408c30eb52e791
SENSENGO\Shelly.Chen <Shelly.Chen@ehsn.com.tw>
2021-06-17 17:36:02 +0800

    Header 頁面切換功能 - 完成

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -2,8 +2,43 @@ import "./Header.scss"
 
 import React, { Component } from "react"
 
+const pageData = [
+  {
+    id: 1,
+    pageName: "reservation-form",
+    title: "預約申請",
+  },
+  {
+    id: 2,
+    pageName: "reserved-list",
+    title: "已預約名單",
+  },
+  {
+    id: 3,
+    pageName: "edit-reservation",
+    title: "編輯預約名單",
+  },
+]
 class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentPageId: 1,
+    }
+    this.onNavLinkClick = this.onNavLinkClick.bind(this)
+    const { changePage } = props
+  }
+
+  onNavLinkClick(e) {
+    const targetId = Number(e.target.id)
+    this.setState({ currentPageId: targetId })
+    this.props.changePage(targetId)
+  }
+
   render() {
+    // const { changePage } = this.props
+
     return (
       <div className="header__wrap">
         <h1 className="text-align-center letter-spacing-20 fz-35 fz-bold-800 padding-b-10 padding-t-10">
@@ -11,33 +46,22 @@ class Header extends Component {
         </h1>
         <nav className="navbar padding-t-15 padding-b-15">
           <ul className="navbar__btns clearfix">
-            <li className="float-left">
-              <a
-                href=""
-                title="疫苗預約"
-                className="btn btn-color-pink-gray letter-spacing-5 padding-10 margin-5 border-radius-12 fz-bold btn-color-pink-gray-active"
-              >
-                疫苗預約
-              </a>
-            </li>
-            <li className="float-left ">
-              <a
-                href=""
-                title="已預約名單"
-                className="btn btn-color-pink-gray letter-spacing-5 padding-10 margin-5 border-radius-12 fz-bold"
-              >
-                已預約名單
-              </a>
-            </li>
-            <li className="float-left">
-              <a
-                href=""
-                title="編輯預約名單"
-                className="btn btn-color-pink-gray letter-spacing-5 padding-10 margin-5 border-radius-12 fz-bold"
-              >
-                編輯預約名單
-              </a>
-            </li>
+            {pageData.map((page) => (
+              <li key={page.id} className="float-left">
+                <button
+                  onClick={this.onNavLinkClick}
+                  id={page.id}
+                  title="疫苗預約"
+                  className={`btn btn-color-pink-gray letter-spacing-5 padding-10 margin-5 border-radius-12 fz-bold fz-20 ${
+                    page.id === this.state.currentPageId
+                      ? "btn-color-pink-gray-active"
+                      : ""
+                  }`}
+                >
+                  {page.title}
+                </button>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -12,13 +12,35 @@ import ReservationForm from "../ReservationForm/ReservationForm"
  * @description 首頁
  */
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentComponentId: 1,
+    }
+    this.changePage = this.changePage.bind(this)
+  }
+
+  changePage(pageId) {
+    this.setState({ currentComponentId: pageId })
+  }
+
   render() {
     return (
       <div className="home d-flex flex-col justify-content-between">
-        <Header />
-        {/* <ReservationForm /> */}
-        <EditReservation />
-        <ReservedList />
+        <Header changePage={this.changePage} />
+
+        {this.state.currentComponentId === 1 ? (
+          <ReservationForm />
+        ) : null}
+
+        {this.state.currentComponentId === 2 ? (
+          <ReservedList />
+        ) : null}
+
+        {this.state.currentComponentId === 3 ? (
+          <EditReservation />
+        ) : null}
+
         <Footer />
       </div>
     )
