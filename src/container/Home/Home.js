8276ab382e41523280ead5dd9acff2229a8ee967
SENSENGO\Shelly.Chen <Shelly.Chen@ehsn.com.tw>
2021-06-21 16:13:43 +0800

    模擬資料 fetching 未成功

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -12,23 +12,26 @@ import ReservationForm from "../ReservationForm/ReservationForm"
  * @description 首頁
  */
 
-const pageData = [
-  {
-    id: 1,
-    pageName: "reservation-form",
-    title: "預約申請",
-  },
-  {
-    id: 2,
-    pageName: "reserved-list",
-    title: "已預約名單",
-  },
-  {
-    id: 3,
-    pageName: "edit-reservation",
-    title: "編輯預約名單",
-  },
-]
+// const pageData = [
+//   {
+//     id: 1,
+//     pageName: "reservation-form",
+//     title: "預約申請",
+//   },
+//   {
+//     id: 2,
+//     pageName: "reserved-list",
+//     title: "已預約名單",
+//   },
+//   {
+//     id: 3,
+//     pageName: "edit-reservation",
+//     title: "編輯預約名單",
+//   },
+// ]
+
+// localStorage.setItem("pageData", JSON.stringify(pageData))
+
 class Home extends Component {
   static defaultProps = {}
 
@@ -36,35 +39,60 @@ class Home extends Component {
     super(props)
 
     this.state = {
-      currentComponentId: pageData[0].id,
+      dataFetchingIsDone: false,
+      pageData: [],
+      currentComponentId: 1,
     }
   }
 
   changePage = (pageId) => {
-    this.setState({
+    this.setState(...this.state, {
       currentComponentId: pageId,
+    })
+  }
+  // componentDidMount() {
+  //   const data = JSON.parse(
+  //     localStorage.getItem("pageData")
+  //   )
+  //   console.log("componentDidMount data", data)
+  //   this.setState({
+  //     pageData: data,
+  //     currentComponentId: this.data[0].id,
+  //   })
+  // }
+
+  componentWillMount() {
+    const data = JSON.parse(
+      localStorage.getItem("pageData")
+    )
+    console.log("componentWillMount data>>>>>", data)
+
+    this.setState(...this.state, {
+      dataFetchingIsDone: true,
+      pageData: data,
+      currentComponentId: data[0].id,
     })
   }
 
   render() {
-    return (
+    return this.dataFetchingIsDone ? (
       <div className="home d-flex flex-col justify-content-between">
         <Header
-          pageData={pageData}
+          pageData={this.pageData}
           changePage={this.changePage}
         />
 
         <ReservationForm
           showElement={
             this.state.currentComponentId ===
-            pageData[0].id
+            this.pageData[0].id
           }
         />
 
         <ReservedList
           showElement={
             this.state.currentComponentId ===
-            pageData[1].id
+            this.pageData[1].id
           }
         />
 
@@ -77,7 +105,7 @@ class Home extends Component {
 
         <Footer />
       </div>
-    )
+    ) : null
   }
 }
 
