ce7657568c8552b9e78e86143bf715cf66b6ff13
SENSENGO\Shelly.Chen <Shelly.Chen@ehsn.com.tw>
2021-07-07 13:37:25 +0800

    修正表單頁 / 編輯頁表單

--- src/component/VaccineReservationForm/VaccineReservationForm.js
+++ src/component/VaccineReservationForm/VaccineReservationForm.js
@@ -22,88 +22,176 @@ const pageData = {
 
 function VaccineReservationForm() {
   return (
-    <div className="reservation-form__wrap">
-      <div className="d-flex justify-content-between">
-        <div className="padding-t-30 padding-l-20">
-          <h1 className="title fz-30 fz-bold display-inline-block">
-            表單標題
-          </h1>
-          <span className="required-mark_content fz-24 padding-l-40">
-            <span className="required-icon">*</span>
-            <span>為必填</span>
-          </span>
-        </div>
+    <div className="reservation-form__wrap contain-width ">
+      <div className="form__header padding-t-60">
+        <h1 className="title fz-35 fz-bold display-inline-block">
+          表單標題
+        </h1>
+        <span className="required-icon padding-l-20">
+          *
+        </span>
+        <span className="required-icon">為必填</span>
         {/* <DeleteIcon /> */}
       </div>
-      <form className="form" method="get">
+      <form
+        // onSubmit={(event) =>
+        //   nowPageId === pageData.reservationForm.id
+        //     ? this.onReserveSubmitBtnClick(event)
+        //     : nowPageId === pageData.editReservation.id
+        //     ? this.onEditSubmitBtnClick(event)
+        //     : this.onReserveSubmitBtnClick(event)
+        // }
+        className="form"
+        method="POST"
+      >
         <div className="input-list">
-          <div className="input-list__right">
-            <div className="name-content">
-              <label htmlFor="name">
-                <span className="required-icon">*</span>
-                姓名
-              </label>
-              <input
-                type="text"
-                name="name"
-                className="input-name input-style required"
-                autoFocus
-              />
-              <p className="display-none">
-                *項目不可以為空
-              </p>
-            </div>
-            <div className="birth-content">
-              <label htmlFor="birth">
-                <span className="required-icon">*</span>
-                生日
-              </label>
-              <input
-                type="text"
-                name="birth"
-                className="birth input-style required"
-                placeholder="範例: 0800101"
-              />
-              <p className="display-none">
-                *項目不可以為空
-              </p>
-            </div>
+          <div className="name-content padding-r-10">
+            <label
+              htmlFor="name"
+              className="display-block"
+            >
+              <span className="required-icon">*</span>
+              姓名
+            </label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              className="input-name input-style"
+              autoFocus
+              // value={fields.name}
+              // onChange={this.handleInputChange}
+            />
+            <p className="fz-16 display-none">
+              *項目不可為空
+            </p>
           </div>
-          <div className="input-list__left">
-            <div className="phone-content">
-              <label htmlFor="phone">
-                <span className="required-icon">*</span>
-                手機號碼
-              </label>
-              <input
-                type="text"
-                name="phone"
-                className="phone input-style required"
-              />
-              <p className="display-none">
-                *項目不可以為空
-              </p>
-            </div>
-            <div className="identityNumber-content">
-              <label htmlFor="identityNumber">
-                <span className="required-icon">*</span>
-                身份證號
-              </label>
-              <input
-                type="text"
-                name="identityNumber"
-                className="identityNumber input-style required "
-              />
-              <p className="display-none">
-                *項目不可以為空
-              </p>
-            </div>
+          <div className="identityNumber-content padding-l-10">
+            <label htmlFor="identity-number">
+              <span className="required-icon">*</span>
+              身份證號
+            </label>
+            <input
+              id="identity-number"
+              type="text"
+              name="identityNumber"
+              className="identityNumber input-style"
+              // value={fields.identityNumber}
+              // onChange={this.handleInputChange}
+              // disabled={
+              //   nowPageId === pageData.editReservation.id
+              // }
+            />
+            <p className="fz-16 display-none">
+              *項目不可為空
+            </p>
+          </div>
+          <div className="birth-content padding-r-10">
+            <label htmlFor="birth">
+              <span className="required-icon">*</span>
+              生日
+            </label>
+            <input
+              id="birth"
+              type="text"
+              name="birth"
+              className="birth input-style"
+              placeholder="範例: 0800101"
+              // value={fields.birth}
+              // onChange={this.handleInputChange}
+            />
+            <p className="fz-16 display-none">
+              *項目不可為空
+            </p>
+            <p className="fz-16">
+              *若您生日為民國80年1月1日，請輸入0800101
+            </p>
+          </div>
+
+          <div className="dayForVaccination-content padding-l-10">
+            <label htmlFor="booking-date">
+              <span className="required-icon">*</span>
+              預約接種日期
+            </label>
+            <input
+              id="booking-date"
+              type="date"
+              name="dayForVaccination"
+              className="booking-date input-style"
+              // value={fields.dayForVaccination}
+              // onChange={this.handleInputChange}
+            />
+            <p className="fz-16 display-none">
+              *項目不可為空
+            </p>
+          </div>
+
+          <div className="phone-content padding-l-10">
+            <label htmlFor="phone">
+              <span className="required-icon">*</span>
+              手機號碼
+            </label>
+            <input
+              id="phone"
+              type="text"
+              name="phone"
+              className="phone input-style"
+              // value={fields.phone}
+              // onChange={this.handleInputChange}
+            />
+            <p className="fz-16 display-none">
+              *項目不可為空
+            </p>
+          </div>
+          <div className="vaccine-content padding-r-10">
+            <label htmlFor="vaccine">
+              <span className="required-icon">*</span>
+              選擇疫苗種類
+            </label>
+            <select
+              id="vaccine"
+              type="text"
+              name="vaccineType"
+              className="vaccine-type input-style"
+              // value={fields.vaccineType}
+              // onChange={this.handleInputChange}
+            >
+              <option value="" disabled>
+                --請選擇--
+              </option>
+              <option value="BNT">BNT</option>
+              <option value="莫德納">莫德納</option>
+              <option value="AZ">AZ</option>
+              <option value="嬌生">嬌生</option>
+              <option value="高端/連雅">
+                高端 / 連雅
+              </option>
+              <option value="科興">科興</option>
+            </select>
+            <p className="fz-16 display-none">
+              *項目不可為空
+            </p>
+          </div>
+          <div className="remark-content">
+            <label htmlFor="remark">備註</label>
+
+            <textarea
+              id="remark"
+              type="text"
+              name="remark"
+              className="remark input-style"
+              // value={fields.remark}
+              // onChange={this.handleInputChange}
+            />
+            <p className="fz-16">
+              *有藥物過敏或特殊病史請填寫備註
+            </p>
           </div>
         </div>
-        <div className="submit-btn-content padding-t-20">
+        <div className="padding-t-20">
           <input
             name="submit-btn"
-            className="input-submit-style btn-submit fz-26  btn-color-pink-white"
+            className="btn-submit fz-26 input-submit-style btn-color-pink-white"
             type="submit"
             value="提 交"
           />
