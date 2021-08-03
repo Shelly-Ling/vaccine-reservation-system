f9abe08c2728862557fc9e734a9507aa78cd19c2
Shelly.Chen <Shelly.Chen@ehsn.com.tw>
2021-08-04 01:02:23 +0800

    完成版HOC範本

--- src/container/HOCPractice/HoverCounter.js
+++ src/container/HOCPractice/HoverCounter.js
@@ -2,25 +2,13 @@ import React, { Component } from "react"
 import UpdateComponent from "./withCounter.js"
 
 class HoverCounter extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      count: 0,
-    }
-  }
-
-  incrementCount = () => {
-    this.setState((prevState) => {
-      return { count: prevState.count + 1 }
-    })
-  }
-
   render() {
+    const { count, incrementCount } = this.props
+
     return (
       <div>
-        <h2 onMouseMove={this.incrementCount}>
-          Hovered {this.state.count} times
+        <h2 onMouseMove={incrementCount}>
+          Hovered {count} times
         </h2>
       </div>
     )
